6bd5e5177db5e8cb02e8f328973f21e1b932b551
ghannoum74 <abdelrahmangh74@gmail.com>
2024-10-23 13:58:56 +0300

    create the home page

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Help from "../components/Help";
+import Navbar from "../components/Navbar";
+
+const HomePage = () => {
+  return (
+    <div className="main-container">
+      <Help />
+      <Navbar />
+    </div>
+  );
+};
+
+export default HomePage;
