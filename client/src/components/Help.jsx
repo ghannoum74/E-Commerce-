32dee2331cbb16f4d120c25b756f0aa2456146f6
ghannoum74 <abdelrahmangh74@gmail.com>
2024-10-23 13:57:55 +0300

    create the help up the navbar

---
+++ client/src/components/Help.jsx
@@ -0,0 +1,12 @@
+const Help = () => {
+  return (
+    <div className="help-container">
+      <p>Find a Store</p>
+      <p>Help</p>
+      <p>Join Us</p>
+      <p>Sign In</p>
+    </div>
+  );
+};
+
+export default Help;
